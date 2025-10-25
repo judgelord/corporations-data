eaccb78a218821803e1e9590c74726c36ece0ec6
stevenhanwen <stevenhanwen.kang@outlook.com>
2025-10-25 02:49:18 -0400

    printing head of files

--- python/main.py
+++ python/main.py
@@ -17,15 +17,18 @@ def load_data(file_path):
         return None
     
 def main():
-    file_path = Path('../data/CIK.csv').resolve()
-    data = load_data(file_path)
-    
-    if data is not None:
-        print("Data loaded successfully:")
-        print(data.head())
-    else:
-        print("Failed to load data.")
-        
-        
+    file_names = ['CIK.csv', 'compustat_clean.csv', 'FDIC_clean.csv', 'SEC_Institutions.csv']  # Add other file names as needed
+    for file_name in file_names:
+        current_dir = Path(__file__).parent
+        file_path = current_dir / '..' / 'data' / file_name
+        file_path = file_path.resolve()
+        data = load_data(file_path)
+
+        if data is not None:
+            print(f"{file_name} data loaded successfully:")
+            print(data.head(20))  # Print first 20 rows
+        else:
+            print(f"Failed to load data from {file_name}.")
+
 if __name__ == "__main__":
     main()
